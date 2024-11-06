94ad0dbea129bd393e5720e5ce0595f466119057
shiron <shiron4710.dev@gmail.com>
2024-11-06 10:18:05 +0900

    feat!: rm v1 project

--- jest.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    "jest/globals": true,
-    node: true,
-  },
-  extends: [require.resolve("./index.js"), "plugin:jest/recommended"],
-};
--- react.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["plugin:react/recommended"],
-  overrides: [],
-  plugins: ["react"],
-  rules: {
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-  },
-};
