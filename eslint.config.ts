6a4ea5038c0d39f06ee00f6cf75c096d27b3df6c
shiron <shiron4710.dev@gmail.com>
2024-11-07 10:00:06 +0900

    feat: add @antfu/eslint-config

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+// @ts-expect-error missing types
+import styleMigrate from "@stylistic/eslint-plugin-migrate"
+
+import shiron from "./src"
+
+export default shiron(
+  {
+    vue: true,
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+    type: "lib",
+  },
+  {
+    ignores: [
+      "fixtures",
+      "_fixtures",
+    ],
+  },
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      "perfectionist/sort-objects": "error",
+    },
+  },
+  {
+    files: ["src/configs/*.ts"],
+    plugins: {
+      "style-migrate": styleMigrate,
+    },
+    rules: {
+      "style-migrate/migrate": ["error", { namespaceTo: "style" }],
+    },
+  },
+)
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import type { TypedFlatConfigItem } from "@antfu/eslint-config"
+import type { Linter } from "eslint"
+import type { Awaitable, FlatConfigComposer } from "eslint-flat-config-utils"
+
+export const shironConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>[] = [
+  {
+    rules: {
+      "style/quotes": [
+        "error",
+        "double",
+      ],
+    },
+  },
+]
